9bca93c9a838fc07af53be0f6037e6fa13c3f0ab
PsheLLVon <pshellvon@gmail.com>
2015-09-25 22:45:01 +0300

    modified:   section_01_(basics)/basic_syntax.py And this are my first try at Cloud.9!

--- section_01_(basics)/basic_syntax.py
+++ section_01_(basics)/basic_syntax.py
@@ -1,5 +1,4 @@
-
-# Basic Syntax
+sic Syntax
 
 # This is a comment.
 # That means Python will let you write notes to yourself and to the other coders checking out your code
@@ -8,15 +7,19 @@
 # The # symbol is what creates a comment.
 # You can have a comment on a line all by itself
 
+#! Okay-okay! I Understand!
+
 # The print statement is a good place to start -- it allows us to see results right away.
 
 print "Or you can have a comment on the same line" # as a command that Python WILL run
 
-print "The print statement will output some text to the screen." # it doesn't print anything to paper.
+print "That my comment. My own!"
 
-print "My first line specialy for my first commit to my OWN git repository" # And also my first comment, huh
+# print "The print statement will output some text to the screen." # it doesn't print anything to paper.
 
 # Python will run commands from top to bottom, left to right
+
+#! Ok, i inderstand!
 
 # So the print statement on line 11 will run before the print statement on line 13
 
@@ -24,10 +27,13 @@ print "My first line specialy for my first commit to my OWN git repository" # An
 
 
 print "You can use lots of newlines to space things out if you like"
+
+print "Myy first ECHO/n"
+
 print "Or you can keep your statements close to one another."
 
 print "It's really up to you, but generally speaking, you'll want to make your code as readable as possible."
-
+print "Okay, a will try to makee my code too many readable as possible"
 # These two statements are identical
 print 4+4
 print 4 + 4
@@ -35,4 +41,5 @@ print 4 + 4
 # Indentation levels matter a lot, even if other kinds of whitespace like newlines or spacing don't matter as much
 
 # So if you uncommented the next line and ran this, you'd get an error.
-print 4 + 4
+#    print 4 + 4
+
